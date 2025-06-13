9e7fbb69cdb1fec636e6d1d69d160ef8548c8298
ChabanovX <firstchabanov@gmail.com>
2025-06-13 18:00:25 +0300

    Add: Finished NavBar UI. Added SVG icons.

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +11,10 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   int _selectedIndex = 0;
 
+  static const _appBarBackgroundColor = Color(0xFF2AE881);
+  static const _inactiveColor = Color(0xFF49454F);
+  static const _indicatorBackgroundColor = Color(0xFFD4FAE6);
+
   static const List<Widget> _widgetOptions = [
     Center(child: Text('Расходы')),
     Center(child: Text('Доходы')),
@@ -20,23 +25,67 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: _widgetOptions.elementAt(_selectedIndex),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.bar_chart), label: 'Расходы'),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.stacked_bar_chart),
-            label: 'Доходы',
-          ),
-          BottomNavigationBarItem(icon: Icon(Icons.calculate), label: 'Счет'),
-          BottomNavigationBarItem(icon: Icon(Icons.article), label: 'Статьи'),
-          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Настройки'),
-        ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-        type: BottomNavigationBarType.fixed,
+    return SafeArea(
+      top: false,
+      bottom: true,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Мои Финансы'),
+          backgroundColor: _appBarBackgroundColor,
+        ),
+        body: _widgetOptions.elementAt(_selectedIndex),
+        bottomNavigationBar: NavigationBar(
+          onDestinationSelected: _onItemTapped,
+          selectedIndex: _selectedIndex,
+          indicatorColor: _indicatorBackgroundColor,
+          destinations: [
+            NavigationDestination(
+              icon: SvgPicture.asset(
+                'assets/icons/expenses.svg',
+                width: 24,
+                height: 24,
+                colorFilter: _selectedIndex == 0 ? null : ColorFilter.mode(_inactiveColor, BlendMode.srcIn),
+              ),
+              label: 'Расходы',
+            ),
+            NavigationDestination(
+              icon: SvgPicture.asset(
+                'assets/icons/incomes.svg',
+                width: 24,
+                height: 24,
+                colorFilter: _selectedIndex == 1 ? null : ColorFilter.mode(_inactiveColor, BlendMode.srcIn),
+              ),
+              label: 'Доходы',
+            ),
+            NavigationDestination(
+              icon: SvgPicture.asset(
+                'assets/icons/account.svg',
+                width: 24,
+                height: 24,
+                colorFilter: _selectedIndex == 2 ? null : ColorFilter.mode(_inactiveColor, BlendMode.srcIn),
+              ),
+              label: 'Счет',
+            ),
+            NavigationDestination(
+              icon: SvgPicture.asset(
+                'assets/icons/articles.svg',
+                width: 24,
+                height: 24,
+                colorFilter: _selectedIndex == 3 ? null : ColorFilter.mode(_inactiveColor, BlendMode.srcIn),
+              ),
+              label: 'Статьи',
+            ),
+            NavigationDestination(
+              icon: SvgPicture.asset(
+                'assets/icons/settings.svg',
+                width: 24,
+                height: 24,
+                colorFilter: _selectedIndex == 4 ? null : ColorFilter.mode(_inactiveColor, BlendMode.srcIn),
+              ),
+              label: 'Настройки',
+            ),
+          ],
+        ),
       ),
     );
   }
