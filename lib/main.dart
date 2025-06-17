1e0622bb8b8613ed16250b5eda9d13a55333277e
ChabanovX <firstchabanov@gmail.com>
2025-06-17 20:15:44 +0300

    Add: added provider state management logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:yndx_homework/presentation/pages/home_page.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: MainApp()));
 }
 
 class MainApp extends StatelessWidget {
