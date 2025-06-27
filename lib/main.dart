226f5e189322cdbb447d46e09138f29b0dda3e75
ChabanovX <firstchabanov@gmail.com>
2025-06-28 02:22:09 +0300

    upd: Logger is enabled

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '/core/log.dart';
 import '/data/datasources/local/objectbox.dart';
 import '/presentation/providers.dart';
 import '/presentation/app.dart' show MainApp;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Log.init();
+
   final objectBox = await ObjectBox.init();
 
   runApp(
