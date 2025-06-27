22667d2a6f128c26dd001b6f51eff24e4aec4f7d
ChabanovX <firstchabanov@gmail.com>
2025-06-28 02:53:39 +0300

    upd: changing brightness keeps state

--- lib/presentation/app.dart
+++ lib/presentation/app.dart
@@ -3,8 +3,27 @@ import 'package:flutter/material.dart';
 import '/core/navigation/app_route_config.dart' show AppRouterDelegate;
 import '/presentation/theme/app_theme.dart' show appTheme, appDarkTheme;
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  late final AppRouterDelegate _routerDelegate;
+
+  @override
+  void initState() {
+    _routerDelegate = AppRouterDelegate();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _routerDelegate.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +33,7 @@ class MainApp extends StatelessWidget {
       darkTheme: appDarkTheme,
       themeMode: ThemeMode.system,
       debugShowCheckedModeBanner: false,
-      routerDelegate: AppRouterDelegate(),
+      routerDelegate: _routerDelegate,
     );
   }
 }
